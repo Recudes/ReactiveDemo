ead78b11c04895ed5232bfddab5f5323e9b53b0d
Raiden <Gordon.Shumway@googlemail.com>
2019-12-05 00:29:28 +0100

    PasswordView without anything

---
+++ PasswordTextfeld.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ReactiveDemo
+{
+    public partial class PasswordTextfeld : Window
+    {
+        public PasswordTextfeld()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PasswordTextfeldViewModel.cs
@@ -0,0 +1,7 @@
+namespace ReactiveDemo
+{
+    public class PasswordTextfeldViewModel
+    {
+        
+    }
+}
